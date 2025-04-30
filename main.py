f6d66a2301d076334e9f5c11ea149cb39372189c
KostyaMc <geimonen68@gmail.com>
2025-04-30 16:59:46 +0300

    added new file schema.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from pydantic import BaseModel, Field
+from schemas import BasePost, User, Post
 from fastapi import FastAPI
-
-
-
-class Post(BaseModel):
-    id: int = Field(..., gt=0)
-    title: str = Field(..., min_length=2, max_length= 50)
-    review: str = Field(..., max_length= 500)
 
 
 app = FastAPI()
@@ -16,7 +9,7 @@ posts = []
 
 # create a new post
 @app.post("/posts")
-def create_post(post: Post):
+def create_post(post: BasePost):
     post.id = len(posts) + 1
     posts.append(post)
     return {"message": "was created a new post", "post": post}
@@ -42,12 +35,10 @@ def rear_post(post_id: int):
 #delete a post
 @app.delete("/posts/{post_id}")
 def delete_post(post_id: int):
-    if post_id <= len(posts):
+    if post_id > len(posts):
+        return {"message": "post not found"}
+    else:
         for post in posts:
             if post.id == post_id:
                 posts.remove(post)
-                return {"message":f"{post.title} was deleted"}
-            else:
-                return {"message": "post not found"}
-    else:
-        return {"message": "post not found"}
+                return {"post": f"{post} was deleted"}
---
+++ schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field, EmailStr, field_validator, ValidationError
+
+
+class BasePost(BaseModel):
+    id: int = Field(..., gt=0)
+    title: str = Field(..., min_length=2, max_length= 50)
+    review: str = Field(..., max_length= 500)
+
+
+class User(BasePost):
+    user_name: str = Field(..., min_length=2, max_length=50)   
+
+    @field_validator("user_name")
+    def name_cannot_be_empty(cls, name):
+        if name == "":
+            raise ValidationError("name cannot be empty")
+        else:
+            return name
+        
+
+    email: EmailStr
+
+
+class Post(BasePost):
+    author: User
